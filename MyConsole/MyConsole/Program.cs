b438c462abd350a827d93c4a7ce273ea1a79bed7
ryabykh2003@gmail.com <ryabykh2003@gmail.com>
2022-10-10 13:40:24 +0300

    Izm

---
+++ MyConsole/MyConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MyLib;
+
+
+namespace MyConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person(35,"Anna");
+            person.Print();
+            person.Print2();
+            person.СheckAge(35);
+
+            }
+
+
+    }
+    }
+
---
+++ MyConsole/MyLib/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyLib
+{
+    public class Person
+    {
+        string name;
+        int age;
+        static int retirementAge = 65;
+        public static int RetirementAge
+        {
+            get { return retirementAge; }
+            set { if (value > 1 && value < 100) retirementAge = value; }
+        }
+        public Person(int age, string name)
+        {
+            this.age = age;
+            this.name = name;
+        }
+        public void СheckAge(int age)
+        {
+            if (age >= retirementAge)
+                Console.WriteLine("Уже на пенсии");
+            else
+                Console.WriteLine($"Сколько лет осталось до пенсии: {retirementAge - age}");
+        }
+        public void Print2() => Console.WriteLine($"Age: {age}");
+        public void Print() => Console.WriteLine($"Name: {name}");
+
+    }
+    
+
+}
